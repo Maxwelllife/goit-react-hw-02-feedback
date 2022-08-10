a1975e941131f10be56d48170ee3e296f2a95a0d
Maksym Pokulytyi <mlightpro@gmail.com>
2022-08-10 22:31:52 +0300

    fix bugs

--- src/components/FeedbackActions/FeedbackActions.jsx
+++ src/components/FeedbackActions/FeedbackActions.jsx
@@ -12,18 +12,6 @@ const FeedbackActions = ({ options, onLeaveFeedback }) => {
         </li>
       ))}
     </ul>
-
-    // <div className={s.buttons}>
-    //   <button onClick={() => onLeaveFeedback('good')} type="button">
-    //     Good
-    //   </button>
-    //   <button onClick={() => onLeaveFeedback('neutral')} type="button">
-    //     Neutral
-    //   </button>
-    //   <button onClick={() => onLeaveFeedback('bad')} type="button">
-    //     Bad
-    //   </button>
-    // </div>
   );
 };
 
